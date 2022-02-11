d43171a900313efcc203bdf1ab1bfeb4de0f23eb
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2022-02-11 09:13:35 +0100

    Reading mirror auth from target namespace
    Instead of having a central place where we configure the mirror registry
    we now we allow uses to configure only the mirror registry URL and when
    importing images we attempt to read secrets for the configured registry
    from within the target namespace (target namespace is where the are
    going to store the Image object).

--- services/sysctx.go
+++ services/sysctx.go
@@ -16,74 +16,27 @@ package services
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
-	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
 	corelister "k8s.io/client-go/listers/core/v1"
-	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/credentialprovider"
+	"k8s.io/kubernetes/pkg/credentialprovider/secrets"
 
-	"github.com/containers/image/v5/docker/reference"
 	"github.com/containers/image/v5/signature"
+	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
-	"github.com/hashicorp/go-multierror"
-	"gopkg.in/yaml.v2"
 
 	"github.com/shipwright-io/image/infra/imagestore"
 )
 
-// We use dockerAuthConfig to unmarshal a default docker configuration present on secrets of
-// type SecretTypeDockerConfigJson. XXX doesn't containers/image export a similar structure?
-// Or maybe even a function to parse a docker configuration file?
-type dockerAuthConfig struct {
-	Auths map[string]types.DockerAuthConfig
-}
-
 // MirrorRegistryConfig holds the needed data that allows imgctrl to contact the mirror registry.
 type MirrorRegistryConfig struct {
-	Address    string
-	Username   string
-	Password   string
-	Repository string
-	Token      string
-	Insecure   bool
-}
-
-// LocalRegistryHostingV1 describes a local registry that developer tools can connect to. A local
-// registry allows clients to load images into the local cluster by pushing to this registry.
-// This is a verbatim copy of what is in the enhancement proposal at
-// https://github.com/kubernetes/enhancements repo
-// keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
-type LocalRegistryHostingV1 struct {
-	// Host documents the host (hostname and port) of the registry, as seen from outside the
-	// cluster. This is the registry host that tools outside the cluster should push images
-	// to.
-	Host string `yaml:"host,omitempty"`
-
-	// HostFromClusterNetwork documents the host (hostname and port) of the registry, as seen
-	// from networking inside the container pods. This is the registry host that tools running
-	// on pods inside the cluster should push images to. If not set, then tools inside the
-	// cluster should assume the local registry is not available to them.
-	HostFromClusterNetwork string `yaml:"hostFromClusterNetwork,omitempty"`
-
-	// HostFromContainerRuntime documents the host (hostname and port) of the registry, as
-	// seen from the cluster's container runtime. When tools apply Kubernetes objects to the
-	// cluster, this host should be used for image name fields. If not set, users of this
-	// field should use the value of Host instead. Note that it doesn't make sense
-	// semantically to define this field, but not define Host or HostFromClusterNetwork. That
-	// would imply a way to pull images without a way to push images.
-	HostFromContainerRuntime string `yaml:"hostFromContainerRuntime,omitempty"`
-
-	// Help contains a URL pointing to documentation for users on how to set up and configure
-	// a local registry. Tools can use this to nudge users to enable the registry.
-	// When possible, the writer should use as permanent a URL as possible to prevent drift
-	// (e.g., a version control SHA). When image pushes to a registry host specified in one of
-	// the other fields fail, the tool should display this help URL to the user. The help URL
-	// should contain instructions on how to diagnose broken or misconfigured registries.
-	Help string `yaml:"help,omitempty"`
+	Address  string
+	Insecure bool
 }
 
 // SysContext groups tasks related to system context/configuration, deal with things such as
@@ -117,47 +70,6 @@ func (s *SysContext) UnqualifiedRegistries(ctx context.Context) ([]string, error
 	return s.unqualifiedRegistries, nil
 }
 
-// ParseMirrorRegistryConfig reads configmap local-registry-hosting from kube-public namespace,
-// parses its content and returns the local registry configuration.
-func (s *SysContext) ParseMirrorRegistryConfig() (*LocalRegistryHostingV1, error) {
-	cm, err := s.cmlister.ConfigMaps("kube-public").Get("local-registry-hosting")
-	if err != nil {
-		return nil, fmt.Errorf("error getting registry configmap: %w", err)
-	}
-
-	dt, ok := cm.Data["localRegistryHosting.v1"]
-	if !ok {
-		return nil, fmt.Errorf("configmap index localRegistryHosting.v1 not found")
-	}
-
-	cfg := &LocalRegistryHostingV1{}
-	if err := yaml.Unmarshal([]byte(dt), cfg); err != nil {
-		return nil, fmt.Errorf("unable to unmarshal registry config: %w", err)
-	}
-	return cfg, nil
-}
-
-// MirrorConfig returns the mirror configuration as read from Shipwright namespace or from the
-// kube-public namespace as per KEP.
-func (s *SysContext) MirrorConfig() (MirrorRegistryConfig, error) {
-	var errors *multierror.Error
-	cfg, err := s.ParseShipwrightMirrorRegistryConfig()
-	if err == nil {
-		return cfg, nil
-	}
-	multierror.Append(errors, err)
-
-	kubecfg, err := s.ParseMirrorRegistryConfig()
-	if err != nil {
-		multierror.Append(errors, err)
-		return MirrorRegistryConfig{}, fmt.Errorf("unable to config mirror: %w", errors)
-	}
-
-	return MirrorRegistryConfig{
-		Address: kubecfg.HostFromContainerRuntime,
-	}, nil
-}
-
 // ParseShipwrightMirrorRegistryConfig parses a secret called "mirror-registry-config" in the pod
 // namespace. This secret holds information on how to connect to the mirror registry.
 func (s *SysContext) ParseShipwrightMirrorRegistryConfig() (MirrorRegistryConfig, error) {
@@ -177,59 +89,9 @@ func (s *SysContext) ParseShipwrightMirrorRegistryConfig() (MirrorRegistryConfig
 	}
 
 	return MirrorRegistryConfig{
-		Address:    string(sct.Data["address"]),
-		Username:   string(sct.Data["username"]),
-		Password:   string(sct.Data["password"]),
-		Repository: string(sct.Data["repository"]),
-		Token:      string(sct.Data["token"]),
-		Insecure:   string(sct.Data["insecure"]) == "true",
+		Address:  string(sct.Data["address"]),
+		Insecure: string(sct.Data["insecure"]) == "true",
 	}, nil
-}
-
-// MirrorRegistryAddresses returns the configured registry address used for mirroring images.
-// This is implemented to comply with KEP at https://github.com/kubernetes/enhancements/ repo,
-// see keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry. There are two
-// ways of providing the mirror registry information, the first one is to populate a secret
-// in the current namespace, the other one is by complying with the KEP. We give preference
-// for the secret in the current namespace.
-func (s *SysContext) MirrorRegistryAddresses() (string, string, error) {
-	var errors *multierror.Error
-	cfg, err := s.ParseShipwrightMirrorRegistryConfig()
-	if err == nil {
-		return cfg.Address, cfg.Address, nil
-	}
-	multierror.Append(errors, err)
-
-	// moves to check through the KEP implementation.
-	kepcfg, err := s.ParseMirrorRegistryConfig()
-	if err != nil {
-		multierror.Append(errors, err)
-		return "", "", fmt.Errorf("mirror registry address unknown: %w", errors)
-	}
-	return kepcfg.HostFromClusterNetwork, kepcfg.HostFromContainerRuntime, nil
-}
-
-// MirrorRegistryContext returns the context to be used when talking to the the registry used
-// for mirroring images.
-func (s *SysContext) MirrorRegistryContext(ctx context.Context) *types.SystemContext {
-	cfg, err := s.ParseShipwrightMirrorRegistryConfig()
-	if err != nil {
-		klog.Infof("unable to read imgctrl mirror registry config: %s", err)
-	}
-
-	insecure := types.OptionalBoolFalse
-	if cfg.Insecure {
-		insecure = types.OptionalBoolTrue
-	}
-
-	return &types.SystemContext{
-		DockerInsecureSkipTLSVerify: insecure,
-		DockerAuthConfig: &types.DockerAuthConfig{
-			Username:      cfg.Username,
-			Password:      cfg.Password,
-			IdentityToken: cfg.Token,
-		},
-	}
 }
 
 // SystemContextsFor builds a series of types.SystemContexts, all of them using one of the auth
@@ -242,18 +104,6 @@ func (s *SysContext) SystemContextsFor(
 	namespace string,
 	insecure bool,
 ) ([]*types.SystemContext, error) {
-	// if imgref points to an image hosted in our mirror registry we return a SystemContext
-	// using default user and pass (the ones user has configured imgctrl with). XXX i am not
-	// sure yet this is a good idea permission wide.
-	domain := reference.Domain(imgref.DockerReference())
-	regaddr, _, err := s.MirrorRegistryAddresses()
-	if err != nil {
-		klog.Infof("no mirror registry configured, moving on")
-	} else if regaddr == domain {
-		mirrorctx := s.MirrorRegistryContext(ctx)
-		return []*types.SystemContext{mirrorctx}, nil
-	}
-
 	auths, err := s.authsFor(ctx, imgref, namespace)
 	if err != nil {
 		return nil, fmt.Errorf("error reading auths: %w", err)
@@ -291,39 +141,35 @@ func (s *SysContext) SystemContextsFor(
 func (s *SysContext) authsFor(
 	ctx context.Context, imgref types.ImageReference, namespace string,
 ) ([]*types.DockerAuthConfig, error) {
-	secrets, err := s.sclister.Secrets(namespace).List(labels.Everything())
+	secsref, err := s.sclister.Secrets(namespace).List(labels.Everything())
 	if err != nil {
 		return nil, fmt.Errorf("fail to list secrets: %w", err)
 	}
 
-	domain := reference.Domain(imgref.DockerReference())
-	if domain == "" {
-		return nil, nil
+	var secs []v1.Secret
+	for _, sct := range secsref {
+		ref := sct.DeepCopy()
+		secs = append(secs, *ref)
 	}
 
+	keyring, err := secrets.MakeDockerKeyring(secs, credentialprovider.NewDockerKeyring())
+	if err != nil {
+		return nil, fmt.Errorf("unable to build keyring: %w", err)
+	}
+
+	imgstr := imgref.DockerReference().String()
+	auths, _ := keyring.Lookup(imgstr)
+
 	var dockerAuths []*types.DockerAuthConfig
-	for _, sec := range secrets {
-		if sec.Type != corev1.SecretTypeDockerConfigJson {
-			continue
-		}
-
-		secdata, ok := sec.Data[corev1.DockerConfigJsonKey]
-		if !ok {
-			continue
-		}
-
-		var cfg dockerAuthConfig
-		if err := json.Unmarshal(secdata, &cfg); err != nil {
-			klog.Infof("ignoring secret %s/%s: %s", sec.Namespace, sec.Name, err)
-			continue
-		}
-
-		sec, ok := cfg.Auths[domain]
-		if !ok {
-			continue
-		}
-
-		dockerAuths = append(dockerAuths, &sec)
+	for _, auth := range auths {
+		dockerAuths = append(
+			dockerAuths,
+			&types.DockerAuthConfig{
+				Username:      auth.Username,
+				Password:      auth.Password,
+				IdentityToken: auth.IdentityToken,
+			},
+		)
 	}
 	return dockerAuths, nil
 }
@@ -338,21 +184,36 @@ func (s *SysContext) DefaultPolicyContext() (*signature.PolicyContext, error) {
 	return signature.NewPolicyContext(pol)
 }
 
-// GetRegistryStore creates an instance of an Registry store entity configured to use our mirror
-// registry as underlying storage.
-func (s *SysContext) GetRegistryStore(ctx context.Context) (*imagestore.Registry, error) {
+// GetRegistryStore creates an instance of a Registry store entity configured to use our mirror
+// registry as underlying storage. This store may vary from one namespace to another as this
+// function sets up authentications (and these are read from the namespace).
+func (s *SysContext) GetRegistryStore(
+	ctx context.Context, namespace, name string,
+) (*imagestore.Registry, error) {
 	defpol, err := s.DefaultPolicyContext()
 	if err != nil {
 		return nil, fmt.Errorf("error reading default policy: %w", err)
 	}
 
-	mcfg, err := s.MirrorConfig()
+	mcfg, err := s.ParseShipwrightMirrorRegistryConfig()
 	if err != nil {
 		return nil, fmt.Errorf("unable to acccess mirror: %w", err)
 	}
 
-	sysctx := s.MirrorRegistryContext(ctx)
-	return imagestore.NewRegistry(mcfg.Address, mcfg.Repository, sysctx, defpol), nil
+	// we create a "fake" image reference pointing to the mirror registry and then
+	// attempt to read authentication for the reference from the target namespace.
+	dststr := fmt.Sprintf("docker://%s/%s/%s", mcfg.Address, namespace, name)
+	imgref, err := alltransports.ParseImageName(dststr)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse reference to mirror: %w", err)
+	}
+
+	auths, err := s.authsFor(ctx, imgref, namespace)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read mirror config: %w", err)
+	}
+
+	return imagestore.NewRegistry(mcfg.Address, auths, mcfg.Insecure, defpol), nil
 }
 
 // RegistriesToSearch returns a list of registries to be used when looking for an image. It is
